ac67a96a826b1a4b622dea2832323160059c8f90
Adam Reis <adamhreis@gmail.com>
2015-08-09 04:09:26 -0400

    Everything working on app engine

--- src/application/views.py
+++ src/application/views.py
@@ -12,10 +12,17 @@ from flask import request, render_template, flash, url_for, redirect
 from flask_cache import Cache
 from application import app
 from models import Post
-import requests
+import urllib2
+import json
 
 # Flask-Cache (configured to use App Engine Memcache API)
 cache = Cache(app)
+
+def request_json(url):
+    req = urllib2.Request(url)
+    response = urllib2.urlopen(req)
+    page = response.read()
+    return json.loads(page)
 
 def warmup():
     """App Engine warmup handler
@@ -25,16 +32,16 @@ def warmup():
     return ''
 
 def home():
-
     return "Sit tight! I'll be up and running soon enough", 200
 
 def scrape():
-    top = requests.get('http://hacker-news.firebaseio.com/v0/topstories.json').json()[:100]
+    top = request_json('https://hacker-news.firebaseio.com/v0/topstories.json')[:100]
     for index, story_id in enumerate(top):
-        story = requests.get('http://hacker-news.firebaseio.com/v0/item/{}.json'.format(story_id))
+        story = request_json('https://hacker-news.firebaseio.com/v0/item/{}.json'.format(story_id))
         
         existingPost = Post.query(Post.url == story.get('url')).fetch()
         if existingPost:
+            existingPost = existingPost[0]
             existingPost.highest_rank = min(index, existingPost.highest_rank)
             existingPost.put()
         else:
